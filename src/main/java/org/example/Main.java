20332312ef139c6b9b53736608f7efcae6535630
Fred <64082311+MatiP911@users.noreply.github.com>
2025-06-13 19:23:06 +0000

    afgdvgb

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,8 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
         System.out.printf("Hell welcome!");
+        int x=5;
+        x=x/0;
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
